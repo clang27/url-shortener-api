88a418577dc2cb217f40abb373bc33bcaf479e72
Collin Lang <collinlang0@gmail.com>
2025-11-06 17:23:36 -0500

    Insert more mock data for analytics

--- database/migration/V1.1__add_mock_data.sql
+++ database/migration/V1.1__add_mock_data.sql
@@ -2,9 +2,27 @@ DO $$
 BEGIN
     IF '${addMockData}' = 'true' THEN
         INSERT INTO url.slugs (slug, target)
-        VALUES ('1fd33d239faf4cd4', 'https://www.google.com');
+        VALUES ('1fd33d239faf4cd4', 'https://www.google.com/');
 
         INSERT INTO url.slugs (slug, target)
-        VALUES ('2fd33d239faf4cd4', 'https://www.yahoo.com');
+        VALUES ('2fd33d239faf4cd4', 'https://www.yahoo.com/');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-11-05 16:52:51.048793');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-11-04 16:52:51.048793');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-11-04 14:52:51.048793');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-11-02 16:52:51.048793');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-11-01 16:52:51.048793');
+
+        INSERT INTO url.redirect_events (slug_id, user_agent, created)
+        VALUES (1, 'Mock Data', '2025-10-25 16:52:51.048793');
     END IF;
 END $$;
